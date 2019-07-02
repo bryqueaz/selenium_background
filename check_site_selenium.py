c3ef83620588ca8979bf222a94ff95cae8b6dca1
bryqueaz <bryanquesa@gmail.com>
2019-07-02 12:17:57 -0600

    Update check_site_selenium.py

--- check_site_selenium.py
+++ check_site_selenium.py
@@ -1,1 +1,21 @@
-
+# coding: utf-8
+import codecs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+##https://selenium-python.readthedocs.io/locating-elements.html
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+driver = webdriver.Chrome('/gms/etc/pluginsconf/selenium_webdriver_chrome/chromedriver', chrome_options=options)
+print ("Headless Chrome Initialized")
+driver.get("https://www.gridshield.com/")
+page = driver.page_source
+file_r = codecs.open('page.html', 'w', encoding='utf8')
+file_r.write(page)
+file_r.close()
